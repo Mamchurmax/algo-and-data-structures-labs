d997c573697f2cc199453f843a5835bac80d53e4
Mamchurmax <maxmamchur6@gmail.com>
2024-02-27 11:10:54 +0200

    apdated acording newest requariments

--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
-def pumpkin(m, n):
-    result = []
-    for i in range(m):
+def pumpkin(array):
+    start_list = []
+    columns = len(array[0])
+    lenght = len(array)
+    final_list = []
+    for i in range(columns):
+        for j in range(lenght):
+            start_list.append(array[j][i])
         if i % 2 == 0:
-            result.extend(list(range(i * n + 1, (i + 1) * n + 1)))
-        else:
-            result.extend(list(range((i + 1) * n, i * n, -1)))
-    return result
+            start_list.reverse()
+        final_list.extend(start_list)
+        start_list = []
+    return final_list
 
 
-print(pumpkin(4, 4))
+arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(pumpkin(arr))

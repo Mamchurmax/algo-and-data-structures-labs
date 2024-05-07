e5dffc8262e690e9c1c20e20c232e145294422e0
Mamchurmax <maxmamchur6@gmail.com>
2024-05-07 09:04:17 +0300

    fixed inputs and tests

--- test/test_islands.py
+++ test/test_islands.py
@@ -7,7 +7,7 @@ class MyTestCase(unittest.TestCase):
         input_file = "C:/Projects/algo-and-data-structures-labs/resources/lab8/islands.csv"
         matrix = read_input(input_file)
         result = min_cable_length(matrix)
-        self.assertEqual(result, 10)
+        self.assertEqual(result, 20)
 
     def test_empty_input(self):
         input_file = "C:/Projects/algo-and-data-structures-labs/resources/lab8/no_islands.csv"
@@ -19,7 +19,7 @@ class MyTestCase(unittest.TestCase):
         input_file = "C:/Projects/algo-and-data-structures-labs/resources/lab8/islands2.csv"
         matrix = read_input(input_file)
         result = min_cable_length(matrix)
-        self.assertEqual(result, 4)
+        self.assertEqual(result, 17)
 
 
 if __name__ == '__main__':

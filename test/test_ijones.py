8b6b61c0fb0eaaf87bccec17e78efb07f772649c
Mamchurmax <maxmamchur6@gmail.com>
2024-05-18 08:34:30 +0300

    fixed tests

--- test/test_ijones.py
+++ test/test_ijones.py
@@ -1,11 +1,16 @@
+import sys
+import os
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.dirname(SCRIPT_DIR))
 from src.ijones import *
 import unittest
 
 
 class MyTestCase(unittest.TestCase):
     def test_given_1(self):
-        input_file = "C:/Projects/algo-and-data-structures-labs/resources/lab9/ijones.in.txt"
-        output_file = "C:/Projects/algo-and-data-structures-labs/resources/lab9/ijones.out.txt"
+        input_file = "../resources/lab9/ijones.in.txt"
+        output_file = "../resources/lab9/ijones.out.txt"
         indiana_jones(input_file, output_file)
         file = open(output_file, 'r')
         result = file.readline()
@@ -13,16 +18,16 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(result, "5")
 
     def test_given_2(self):
-        input_file = "C:/Projects/algo-and-data-structures-labs/resources/lab9/ijones2.in.txt"
-        output_file = "C:/Projects/algo-and-data-structures-labs/resources/lab9/ijones2.out.txt"
+        input_file = "../resources/lab9/ijones2.in.txt"
+        output_file = "../resources/lab9/ijones2.out.txt"
         indiana_jones(input_file, output_file)
         with open(output_file, 'r') as f:
             result = f.readline()
         self.assertEqual(result, "2")
 
     def test_given_3(self):
-        input_file = "C:/Projects/algo-and-data-structures-labs/resources/lab9/ijones3.in.txt"
-        output_file = "C:/Projects/algo-and-data-structures-labs/resources/lab9/ijones3.out.txt"
+        input_file = "../resources/lab9/ijones3.in.txt"
+        output_file = "../resources/lab9/ijones3.out.txt"
         indiana_jones(input_file, output_file)
         with open(output_file, 'r') as f:
             result = f.readline()
